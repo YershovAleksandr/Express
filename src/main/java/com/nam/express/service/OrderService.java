fb07f41aac7b2df82e381c972a345475ac9f25e9
hp <namstudionsk@gmail.com>
2019-02-10 23:39:21 +0700

    Add Order Service

---
+++ src/main/java/com/nam/express/service/OrderService.java
@@ -0,0 +1,18 @@
+package com.nam.express.service;
+
+import com.nam.express.dao.OrderDao;
+import com.nam.express.model.Order;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class OrderService {
+    private static Logger log = LoggerFactory.getLogger(OrderService.class);
+    private static OrderDao orderDao = new OrderDao();
+
+    public static void createOrder(Order order){
+        log.info("Create Order");
+
+        orderDao.create(order);
+    }
+
+}
