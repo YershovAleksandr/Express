e7fac94e2186cedcd0f611a28d858a72513db372
hp <namstudionsk@gmail.com>
2019-02-10 23:37:54 +0700

    Fix

--- src/main/java/com/nam/express/Main.java
+++ src/main/java/com/nam/express/Main.java
@@ -1,11 +1,14 @@
 package com.nam.express;
 
+import com.nam.express.util.Database;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class Main {
     public static void main(String[] args){
+        Database.init();
+
         SpringApplication.run(Main.class, args);
     }
 }
