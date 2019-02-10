887e0d713757179198cac317469ca2c88437508c
hp <namstudionsk@gmail.com>
2019-02-11 00:53:56 +0700

    Refactoring Task dao for mysql

--- src/main/java/com/nam/express/dao/CourierTaskDao.java
+++ src/main/java/com/nam/express/dao/CourierTaskDao.java
@@ -1,41 +1,79 @@
 package com.nam.express.dao;
 
 import com.nam.express.model.CourierTask;
+import com.nam.express.util.DataSource;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.util.ArrayList;
 import java.util.List;
 
 public class CourierTaskDao {
     private static Logger log = LoggerFactory.getLogger(CourierTaskDao.class);
 
-    private static List<CourierTask> courierTaskList = new ArrayList<>();
-
     public List<CourierTask> getAll(){
+        log.info("Get All Courier Tasks");
 
-/*        CourierTask courierTask = new CourierTask();
-        courierTask.setId(1);
-        courierTask.setDescription("Description 1");
-        courierTask.setOrderId(1);
-        courierTaskList.add(courierTask);
+        List<CourierTask> courierTaskList = null;
 
-        CourierTask courierTask2 = new CourierTask();
-        courierTask2.setId(2);
-        courierTask2.setDescription("Description 2");
-        courierTask2.setOrderId(3);
-        courierTaskList.add(courierTask2);
-*/
+        try(Connection con = DataSource.getConnection()) {
+            PreparedStatement pst = con.prepareStatement("SELECT * FROM courierdb");
+            ResultSet rs = pst.executeQuery();
+
+            courierTaskList = new ArrayList<>();
+
+            while(rs.next()){
+                CourierTask courierTask = new CourierTask();
+                courierTask.setId(rs.getInt("courierdb_id"));
+                courierTask.setOrderId(rs.getInt("courierdb_orderid"));
+                courierTask.setDescription(rs.getString("courierdb_description"));
+
+                courierTaskList.add(courierTask);
+            }
+        }catch (Exception e){
+            log.error("DB Error", e);
+        }
+
         return courierTaskList;
     }
 
-    public void create(CourierTask courierTask){
+    public int create(CourierTask courierTask){
         log.info("Create Courier Task");
 
-        courierTaskList.add(courierTask);
+        int status = -1;
+
+        try(Connection con = DataSource.getConnection()){
+            PreparedStatement pst = con.prepareStatement("INSERT INTO courierdb(courierdb_orderid, courierdb_description) VALUES (?, ?)");
+
+            pst.setInt(1, courierTask.getOrderId());
+            pst.setString(2, courierTask.getDescription());
+
+            status = pst.executeUpdate();
+        }catch (Exception e){
+            log.error("DB Error", e);
+        }
+
+        return status;
     }
 
-    public void delete(int id){
+    public int delete(int id){
+        log.info("Delete Courier Task");
 
+        int status = -1;
+
+        try(Connection con = DataSource.getConnection()){
+            PreparedStatement pst = con.prepareStatement("DELETE FROM courierdb WHERE courierdb_id = ?");
+
+            pst.setInt(1, id);
+
+            status = pst.executeUpdate();
+        }catch (Exception e){
+            log.error("DB Error", e);
+        }
+
+        return status;
     }
 }
