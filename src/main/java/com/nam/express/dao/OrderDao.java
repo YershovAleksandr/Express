beda42413597e4b6ecfd187843bf42fbfcfa91c1
hp <namstudionsk@gmail.com>
2019-02-10 23:39:12 +0700

    Add Order DAO

---
+++ src/main/java/com/nam/express/dao/OrderDao.java
@@ -0,0 +1,17 @@
+package com.nam.express.dao;
+
+import com.nam.express.model.Order;
+
+import java.util.List;
+
+public class OrderDao {
+
+    /*public List<Order> getAll(){
+
+        return null;
+    }*/
+
+    public void create(Order order){
+
+    }
+}
