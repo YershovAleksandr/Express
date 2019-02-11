68e2b92dde101e2ac9e28bcf35f542be8ec49f2d
hp <namstudionsk@gmail.com>
2019-02-12 02:03:04 +0700

    Fix

--- src/main/java/com/nam/express/Main.java
+++ src/main/java/com/nam/express/Main.java
@@ -1,17 +1,11 @@
 package com.nam.express;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class Main {
-    private static Logger log = LoggerFactory.getLogger(Main.class);
-
     public static void main(String[] args){
-       // Database.init();
-
         SpringApplication.run(Main.class, args);
     }
 }
