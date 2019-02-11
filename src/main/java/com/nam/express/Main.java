812a9d9baa1bc73a3ee9173979dcb7d396bd8e74
hp <namstudionsk@gmail.com>
2019-02-11 23:54:08 +0700

    Fix

--- src/main/java/com/nam/express/Main.java
+++ src/main/java/com/nam/express/Main.java
@@ -1,10 +1,12 @@
 package com.nam.express;
 
 import com.nam.express.util.Database;
+import com.nam.express.util.JDBCSource;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.jdbc.core.JdbcTemplate;
 
 @SpringBootApplication
 public class Main {
@@ -14,5 +16,14 @@ public class Main {
         Database.init();
 
         SpringApplication.run(Main.class, args);
+
+        //JDBCSource s = new JDBCSource();
+        //ds.mysqlDataSource();
+        JdbcTemplate jdbcTemplate = new JdbcTemplate(JDBCSource.getDataSource());
+        //JdbcTemplate jdbcTemplate = new JdbcTemplate();
+
+        int i = jdbcTemplate.queryForObject("Select count(*) from orderdb", Integer.class);
+
+        System.out.println("I = " + i);
     }
 }
