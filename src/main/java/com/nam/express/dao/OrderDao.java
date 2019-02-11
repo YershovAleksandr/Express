9fa581cc465218a34277c6b4f2ec7ddccbc3022a
hp <namstudionsk@gmail.com>
2019-02-12 00:05:30 +0700

    Refactoring for use jdbctemplate

--- src/main/java/com/nam/express/dao/OrderDao.java
+++ src/main/java/com/nam/express/dao/OrderDao.java
@@ -1,20 +1,18 @@
 package com.nam.express.dao;
 
 import com.nam.express.model.Order;
-import com.nam.express.util.DataSource2;
 import com.nam.express.util.JDBCSource;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.jdbc.core.simple.SimpleJdbcInsert;
 
-import java.sql.Connection;
-import java.sql.PreparedStatement;
-import java.sql.ResultSet;
 import java.util.HashMap;
 import java.util.Map;
 
 public class OrderDao {
     private static Logger log = LoggerFactory.getLogger(OrderDao.class);
+    private JdbcTemplate jdbcTemplate = new JdbcTemplate(JDBCSource.getDataSource());
 
     public void create(Order order){
         log.info("Create Order");
@@ -30,24 +28,6 @@ public class OrderDao {
     public Order get(int id){
         log.info("Get Order id = " + id);
 
-        Order order = null;
-
-        try(Connection con = DataSource2.getConnection()) {
-            PreparedStatement pst = con.prepareStatement("SELECT * FROM orderdb WHERE orderdb_id = ?");
-            pst.setInt(1, id);
-
-            ResultSet rs = pst.executeQuery();
-
-            if(rs.next()){
-                order = new Order();
-
-                order.setId(rs.getInt("orderdb_id"));
-                order.setDescription(rs.getString("orderdb_description"));
-            }
-        }catch (Exception e){
-            log.error("DB Error", e);
-        }
-
-        return order;
+        return jdbcTemplate.queryForObject("SELECT * FROM orderdb WHERE orderdb_id = ?", new Object[]{id}, new OrderMapper());
     }
 }
