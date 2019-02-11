969290d48a980740ed9b1cf610b423c96f30866b
hp <namstudionsk@gmail.com>
2019-02-12 02:02:04 +0700

    Add data.sql

---
+++ src/main/resources/db/data.sql
@@ -0,0 +1,12 @@
+INSERT INTO orderdb (orderdb_description) VALUES ('11');
+INSERT INTO orderdb (orderdb_description) VALUES ('22');
+INSERT INTO orderdb (orderdb_description) VALUES ('33');
+INSERT INTO orderdb (orderdb_description) VALUES ('44');
+INSERT INTO orderdb (orderdb_description) VALUES ('55');
+
+INSERT INTO courierdb (courierdb_orderid, courierdb_description) VALUES (1, 'Оставить на вахте');
+INSERT INTO courierdb (courierdb_orderid, courierdb_description) VALUES (3, 'В бухгалтерию');
+INSERT INTO courierdb (courierdb_orderid, courierdb_description) VALUES (4, 'Лично в руки директору!!! (Секретарю не отдавать!!)');
+INSERT INTO courierdb (courierdb_orderid, courierdb_description) VALUES (5, '3-ий подъезд');
+INSERT INTO courierdb (courierdb_orderid, courierdb_description) VALUES (2, 'Калитка справа, 2-ой этаж');
+
